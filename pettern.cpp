a52046ae792fb00bf539b2165b01e97cc5360710
vishal1202verma <vishalvermao.pcm121@gmail.com>
2023-10-19 12:53:34 +0530

    first day challenge commit

---
+++ pettern.cpp
@@ -0,0 +1,16 @@
+// simpe pattern questo on loops 
+#include<iostream>
+using namespace std;
+int main(){
+int n = 4;
+int m = 5;
+ for( int i=1; i<=m; i++){
+    for(int j=1; j<=m; j++){
+        cout<<"*";
+    }
+    cout<<endl;
+ }
+
+
+
+}

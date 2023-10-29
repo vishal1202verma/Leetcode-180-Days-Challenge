552954d9577cd8f5fa42ffbe0867eec081532490
vishal1202verma <vishalvermao.pcm121@gmail.com>
2023-10-29 22:05:24 +0530

    function commited

---
+++ Functions/Arguments.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int fun(int x, int y){
+    return x+y;
+}
+
+
+int main (){
+
+cout<<fun( 3, 5)<<endl;
+cout<<fun( 6, 7)<<endl;
+cout<<fun( 8, 9)<<endl;
+
+
+
+
+}

1a1063f5c15f75d2d914626bf2c5b3d912046e8f
vishal1202verma <vishalvermao.pcm121@gmail.com>
2023-11-01 19:54:58 +0530

    pointers commited

---
+++ Pointers/SumTwoNum.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main (){   
+ int a, b;
+ cin>>a>>b;
+ int *p1 = &a;
+ int *p2 = &b;
+ int sum = *p1 + *p2;
+ cout<<sum;
+ 
+
+}
---
+++ Pointers/SwapbyPointer.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+void swap (int &a, int &b){
+ int temp  =a;
+    a = b;
+    b = temp;
+    return;
+    
+}
+int main (){
+int a ,b;
+cout<<"the value of a and b :";
+cin>>a>>b;
+swap(a ,b);
+cout<<a<<" "<<b; 
+ 
+
+
+
+
+}

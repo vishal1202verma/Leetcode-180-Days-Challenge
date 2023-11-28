f71f8e543b7115a6d2272c893fce0856f7b4f1c9
vishal1202verma <vishalvermao.pcm121@gmail.com>
2023-11-28 22:13:04 +0530

    commited

---
+++ Array2D/Array2DPart-1/Max.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main (){
+int arr[3] ={3,3,4};
+int max= arr[0];
+for(int i=0; i<3; i++){
+    if(max<arr[i]){
+        max = arr[i];
+    }
+}
+cout<<max;
+cout<<"vishal verma ji";
+
+
+
+}

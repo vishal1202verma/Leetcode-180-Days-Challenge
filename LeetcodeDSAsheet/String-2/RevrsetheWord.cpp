20344b877635ae23bd450b24e145669efd723656
vishal1202verma <vishalvermao.pcm121@gmail.com>
2023-12-13 12:22:09 +0530

    leetcode 151 commited

---
+++ LeetcodeDSAsheet/String-2/RevrsetheWord.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<stringstream>
+using namespace std;
+int main (){
+string s = "the sky is blue";
+ vector<string> words;
+        stringstream ss(s);
+        string tmp;
+        while (ss >> tmp)
+            words.push_back(tmp);
+
+        string ans;
+        for (int i = words.size() - 1; i >= 0; --i) {
+            if (i != words.size() - 1) ans += " ";
+            ans += words[i];
+        }
+        cout<<ans;
+
+}

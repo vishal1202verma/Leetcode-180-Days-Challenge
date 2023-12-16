81655c73130ec9c1d1a9e2d3a20d680e9bb1b3ea
vishal1202verma <vishalvermao.pcm121@gmail.com>
2023-12-16 11:31:06 +0530

    rotate the array commited

--- LeetcodeDSAsheet/String-2/RotateString.cpp
+++ LeetcodeDSAsheet/String-2/RotateString.cpp
@@ -1,24 +1,41 @@
 #include<iostream>
 #include<algorithm>
+#include<string>
+#include<sstream>
+#include<vector>
 using namespace std;
-bool rotateString(string s, string goal) {
-        int n  = s.size();
-        int m  = goal.size();
-        for(int i=0; i<s.size(); i++){
-
-        if(n==m){
-            return true;
-        }
-        else if (s[i] != goal[i]){
-            return false;
-        }
-        }
-        return {};
-    }
-
-
 int main (){
+string s = "abcde";
+string goal = "cdeab";
+int n  = s.size();
+int m  = goal.size();
+        // int i=0; 
+        // int j= n-1;
+        // while(i<j){
+        //     char temp = s[j];
+        //     s[j] = s[i];
+        //     s[i] = temp;
+        //     i++;
+        //     j--;
+        // }
+       string last = "";
+       for(int i=2;  i<n; i ++){
+           last += s[i];
+       } 
+       cout<<last;
+       cout<<endl;
+       string temp = "";
+        for(int i=0;  i<n-3; i ++){
+           temp += s[i];
+       } 
+       cout<<temp;
+       cout<<endl;
+       string ans = last+temp;
+       cout<<ans;
+      
 
+
+       
 
 
 }
---
+++ LeetcodeDSAsheet/String-2/tempCodeRunnerFile.cpp
@@ -0,0 +1,9 @@
+ // int i=0; 
+        // int j= n-1;
+        // while(i<j){
+        //     char temp = s[j];
+        //     s[j] = s[i];
+        //     s[i] = temp;
+        //     i++;
+        //     j--;
+        // }

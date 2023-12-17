2e86aa529720d4eb2db61da36df45378af18ac80
vishal1202verma <vishalvermao.pcm121@gmail.com>
2023-12-17 11:07:32 +0530

    commited

--- LeetcodeDSAsheet/String-2/RotateString.cpp
+++ LeetcodeDSAsheet/String-2/RotateString.cpp
@@ -9,30 +9,27 @@ string s = "abcde";
 string goal = "cdeab";
 int n  = s.size();
 int m  = goal.size();
-        // int i=0; 
-        // int j= n-1;
-        // while(i<j){
-        //     char temp = s[j];
-        //     s[j] = s[i];
-        //     s[i] = temp;
-        //     i++;
-        //     j--;
-        // }
-       string last = "";
-       for(int i=2;  i<n; i ++){
-           last += s[i];
-       } 
-       cout<<last;
-       cout<<endl;
-       string temp = "";
-        for(int i=0;  i<n-3; i ++){
-           temp += s[i];
-       } 
-       cout<<temp;
-       cout<<endl;
-       string ans = last+temp;
-       cout<<ans;
-      
+    //    string last = "";
+    //    for(int i=2;  i<n; i ++){
+    //        last += s[i];
+    //    } 
+    //    cout<<last;
+    //    cout<<endl;
+    //    string temp = "";
+    //     for(int i=0;  i<n-3; i ++){
+    //        temp += s[i];
+    //    } 
+    //    cout<<temp;
+    //    cout<<endl;
+    //    string ans = last+temp;
+    //    cout<<ans;
+         int k=0;
+        for(int k=0;k<s.size();k++){
+            reverse(s.begin(),s.end()-1);
+            reverse(s.begin(),s.end());
+            if(s==goal)return true;
+        }
+        return false;
 
 
        
